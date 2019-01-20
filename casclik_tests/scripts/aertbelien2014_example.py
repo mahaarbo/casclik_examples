dbe21a57db65c74cbdebbf5a381973d2b8f310f1
Mathias Hauan Arbo <mathias.arbo@itk.ntnu.no>
2019-01-20 20:58:52 +0100

    ns->namespace

--- casclik_tests/scripts/aertbelien2014_example.py
+++ casclik_tests/scripts/aertbelien2014_example.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     resp = robintrfc.switch_hw_controller(  # sane switching of controllers
         "joint_position_controller",
         resources=joint_names,
-        ns="")
+        namespace="")
 
     if resp.ok == 1:
         rospy.loginfo("Controller switched!")
