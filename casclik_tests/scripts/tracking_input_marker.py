bc2c8139e0d0c0b7d8c85adbb7f62eb1f6b013f8
Mathias Hauan Arbo <mathias.arbo@itk.ntnu.no>
2019-01-20 21:25:22 +0100

    ns->namespace

--- casclik_tests/scripts/tracking_input_marker.py
+++ casclik_tests/scripts/tracking_input_marker.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     resp = robintrfc.switch_hw_controller(  # sane switching of controllers
         "joint_position_controller",
         resources=fk_dict["joint_names"],
-        ns="")
+        namespace="")
 
     if resp.ok == 1:
         rospy.loginfo("Controller switched!")
